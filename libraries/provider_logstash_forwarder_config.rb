daafcd5c96d68e66b3f21750090d15b1a12c552d
Jacob Dearing <jacob.dearing@gmail.com>
2015-07-11 05:52:47 -0500

    expose attributes for templating

--- libraries/provider_logstash_forwarder_config.rb
+++ libraries/provider_logstash_forwarder_config.rb
@@ -6,6 +6,35 @@ class Chef
       provides :logstash_forwarder_config
 
       action :create do
+        template '/etc/init.d/logstash-forwarder' do
+          source 'initd/logstash-forwarder.initd.erb'
+          owner 'root'
+          group 'root'
+          mode '0755'
+          cookbook new_resource.source
+          variables options: {
+            'user' => new_resource.user,
+            'group' => new_resource.group,
+            'chroot' => new_resource.chroot,
+            'chdir' => new_resource.chdir,
+            'nice' => new_resource.nice
+          }
+        end
+
+        template '/etc/default/logstash-forwarder' do
+          source 'logstash-forwarder.erb'
+          owner 'root'
+          group 'root'
+          mode '0644'
+          cookbook new_resource.source
+          variables options: {
+            'user' => new_resource.user,
+            'group' => new_resource.group,
+            'chroot' => new_resource.chroot,
+            'chdir' => new_resource.chdir,
+            'nice' => new_resource.nice
+          }
+        end
       end
 
       action :delete do
--- libraries/resource_logstash_forwarder_config.rb
+++ libraries/resource_logstash_forwarder_config.rb
@@ -6,6 +6,14 @@ class Chef
       resource_name :logstash_forwarder_config
       default_action :create
       actions [:create, :delete]
+
+      attribute :source, kind_of: String, default: 'elk'
+
+      attribute :user, kind_of: String, default: 'root'
+      attribute :group, kind_of: String, default: 'root'
+      attribute :chroot, kind_of: String, default: '/'
+      attribute :chdir, kind_of: String, default: 'var/lib/logstash-forwarder'
+      attribute :nice, kind_of: String, default: ''
     end
   end
 end
