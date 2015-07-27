f706310f50100c654723726cfefe5f4009c5b233
Jacob Dearing <jacob.dearing@gmail.com>
2015-07-27 11:40:52 -0500

    cosmetic elktest changes

--- test/cookbooks/elktest/recipes/default.rb
+++ test/cookbooks/elktest/recipes/default.rb
@@ -1,9 +1,9 @@
 
-# test a single node with all three projects
+# test a single node with all four projects
 
-package 'java-1.8.0-openjdk-headless'
-package 'tar'
-package 'nano'
+%w( tar nano htop java-1.8.0-openjdk-headless).each do |pkg|
+  package pkg
+end
 
 include_recipe 'runit::default'
 include_recipe 'elktest::elasticsearch'
