a0dbc6cf56876e6268c2778540b16eb080827f91
Jacob Dearing <jacob.dearing@gmail.com>
2015-07-27 16:42:23 -0500

    simplify testing cookbook

--- test/cookbooks/elktest/recipes/default.rb
+++ test/cookbooks/elktest/recipes/default.rb
@@ -1,12 +1,31 @@
-
 # test a single node with all four projects
-
 %w( tar nano htop java-1.8.0-openjdk-headless).each do |pkg|
   package pkg
 end
 
 include_recipe 'runit::default'
-include_recipe 'elktest::elasticsearch'
-# include_recipe 'elktest::logstash'
-include_recipe 'elktest::logstash_forwarder'
-include_recipe 'elktest::kibana'
+
+# create certs (subject = localhost)
+secrets = Chef::DataBagItem.load('secrets', 'logstash')
+logstash_key = Base64.decode64(secrets['key'])
+file '/tmp/logstash.key' do
+  content logstash_key
+end
+logstash_crt = Base64.decode64(secrets['certificate'])
+file '/tmp/logstash.crt' do
+  content logstash_crt
+end
+
+elasticsearch 'default'
+
+logstash 'default' do
+  crt_location '/tmp/logstash.crt'
+  key_location '/tmp/logstash.key'
+end
+
+logstash_forwarder 'default' do
+  crt_location '/tmp/logstash.crt'
+  key_location '/tmp/logstash.key'
+end
+
+kibana 'default'
