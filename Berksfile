c3206425e46e36b51885a06ce6e5c831c929d409
Jacob Dearing <jacob.dearing@gmail.com>
2015-07-31 18:48:39 -0500

    version bump

--- Berksfile
+++ Berksfile
@@ -3,7 +3,6 @@ source 'https://supermarket.chef.io'
 metadata
 
 group :integration do
-  cookbook 'yum'
   cookbook 'elktest', path: './test/cookbooks/elktest'
   cookbook 'runit', '~> 1.7.2'
 end
--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,9 @@ maintainer_email 'jacob.dearing@gmail.com'
 license 'MIT'
 description 'Installs/Configures ELK'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.1'
+version '0.2.2'
 
 supports 'centos'
 
-depends 'yum'
 depends 'ark'
 depends 'runit'
