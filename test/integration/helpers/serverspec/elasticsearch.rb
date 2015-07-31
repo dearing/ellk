ec364c1597aa4751288a56afc06004a5f3d10b62
Jacob Dearing <jacob.dearing@gmail.com>
2015-07-31 17:27:22 -0500

    update testing for env vars and api checks

--- test/integration/helpers/serverspec/elasticsearch.rb
+++ test/integration/helpers/serverspec/elasticsearch.rb
@@ -1,10 +1,21 @@
 # sshd_service = os[:family] == 'redhat' ? 'sshd' : 'ssh'
 
 shared_examples 'elasticsearch' do
+
   describe command('sv status elasticsearch') do
     its(:stdout) { should match 'run: elasticsearch' }
   end
-end
 
-# curl -X GET http://localhost:9200/
-# curl 'localhost:9200/_cat/health?v'
+  describe command('curl http://localhost:9200/') do
+    its(:stdout) { should match '"status" : 200,' }
+  end
+
+  describe command('curl http://localhost:9200/_cat/health?v') do
+    its(:stdout) { should match 'elasticsearch' }
+  end
+
+  describe file('/etc/service/elasticsearch/env/ES_USER') do
+  	it { should be_file }
+  	its(:content) { should match 'elasticsearch' }
+  end
+end
