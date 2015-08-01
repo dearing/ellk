bf00fccb2e7c0e6c189dedc3b6e2b31f8de227a3
Jacob Dearing <jacob.dearing@gmail.com>
2015-08-01 05:08:50 -0500

    revert metabump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jacob.dearing@gmail.com'
 license 'MIT'
 description 'Installs/Configures ELLK'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.3.2'
+version '0.3.1'
 
 %w(ubuntu debian gentoo centos redhat amazon scientific oracle enterpriseenterprise).each do |os|
   supports os
