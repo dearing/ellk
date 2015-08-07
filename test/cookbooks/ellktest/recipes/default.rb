13e9fea02ad216e3a8757151da1d0b5156f0c93b
Jacob Dearing <jacob.dearing@gmail.com>
2015-08-07 03:58:09 -0500

    update comments and demonstrate logstash-forwarder files attribute

--- test/cookbooks/ellktest/recipes/default.rb
+++ test/cookbooks/ellktest/recipes/default.rb
@@ -33,7 +33,7 @@ file '/tmp/logstash.crt' do
   content logstash_crt
 end
 
-## ELASTICSEARCH
+## install ELASTICSEARCH and configure to use tmp for data storage
 elasticsearch 'default' do
   datadir '/tmp/es_datadir'
   version '1.7.1'
@@ -41,7 +41,7 @@ elasticsearch 'default' do
   url 'https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.7.1.tar.gz'
 end
 
-## LOGSTASH
+## install LOGSTASH and source my ellktest's templates instead
 bonus_env = { 'HELLO' => 'WORLD', 'LS_USER' => 'kibana' } # for testing sake
 bonus_conf = { 'test_value' => 'merged hash from a recipe!' }
 logstash 'default' do
@@ -52,14 +52,17 @@ logstash 'default' do
   conf_options bonus_conf
 end
 
-## LOGSTASH-FORWARDER
 logstash_forwarder 'default' do
   crt_location '/tmp/logstash.crt'
   logstash_servers ['localhost:5043']
-  files [{ 'paths' => ['/var/log/messages', '/var/log/*log'], 'fields' => { 'type' => 'syslog' } }]
+  files [
+    {
+      'paths' => ['/var/log/messages', '/var/log/*log'], 'fields' => { 'type' => "syslog-#{node.chef_environment}" }
+    }
+  ]
 end
 
-## KIBANA
+## install KIBANA and configure for port 8080, maybe we'll proxy to it from NGINX with some auth_basic?
 kibana 'default' do
   port 8080
 end
