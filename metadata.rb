d09b41c33b935de72d8fea812fb1a0022c4924c3
Jacob Dearing <jacob.dearing@gmail.com>
2015-10-15 12:50:36 -0500

    update cookbook version and remove runit pinning

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,11 @@ maintainer_email 'jacob.dearing@gmail.com'
 license 'MIT'
 description 'Library to handle Elasticsearch, Logstash, Logstash-Forwarder & Kibana'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.3.4'
+version '0.3.5'
 
 %w(ubuntu debian centos redhat amazon scientific oracle enterpriseenterprise).each do |os|
   supports os
 end
 
 depends 'ark'
-depends 'runit', '= 1.5.18'
+depends 'runit'
