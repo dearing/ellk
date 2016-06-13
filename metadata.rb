a248ce4942330bd2828b41a55b63249b6b4e3479
Jacob Dearing <jacob.dearing@gmail.com>
2016-06-13 02:53:30 -0500

    rubocop fix
    attempt cacheing
    
    use bundler for calls

--- metadata.rb
+++ metadata.rb
@@ -9,7 +9,6 @@ version '0.3.6'
 issues_url 'https://github.com/dearing/ellk/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/dearing/ellk' if respond_to?(:source_url)
 
-
 %w(ubuntu debian centos redhat amazon scientific oracle enterpriseenterprise).each do |os|
   supports os
 end
