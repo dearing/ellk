9db524a4694134c3f22f4bd9e9d6b19f1616beff
Jacob Dearing <jacob.dearing@gmail.com>
2016-06-13 02:51:35 -0500

    update metadata for FC064 & FC065

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,10 @@ description 'Library to handle Elasticsearch, Logstash, Logstash-Forwarder & Kib
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.3.6'
 
+issues_url 'https://github.com/dearing/ellk/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/dearing/ellk' if respond_to?(:source_url)
+
+
 %w(ubuntu debian centos redhat amazon scientific oracle enterpriseenterprise).each do |os|
   supports os
 end
